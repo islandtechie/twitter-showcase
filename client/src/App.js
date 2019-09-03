ef535c9f0ea398954bd18afdb4159fc2aec0bcbc
Berlin Smith <berlin.smithjr@gmail.com>
2019-09-03 18:00:59 -0400

    Completed Home.js markup

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,27 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom'
+import './App.css';
+import Home from './pages/Home';
 
 class App extends Component {
+    state = {
+
+    }
+
     render() {
         return (
-            <div>
-                
-            </div>
+            <Fragment>
+                <Router>
+                    <nav>
+                        <NavLink to='/'>Home</NavLink>
+                        <NavLink to='/user-search'>User Search</NavLink>
+                        <NavLink to='/random-search'>Random Search</NavLink>
+                    </nav>
+                    <div className="content">
+                        <Route path="/" exact component={Home} />
+                    </div>
+                </Router>
+            </Fragment>
         )
     }
 }
---
+++ client/src/pages/Home.js
@@ -0,0 +1,48 @@
+import React, { Component, Fragment } from 'react'
+
+class Home extends Component {
+    render() {
+        return (
+            <Fragment>
+                <div className="logo">
+                    <img src="_assets/t-logo.png" alt=""/>
+                </div>
+                <div className="search-area">
+                    <div className="tweet">
+                        <div className="tweet_avatar">
+                            <img src="_assets/t-logo.png" alt=""/>
+                        </div>
+                        <div className="tweet__body">
+                            <div className="tweet_body_header">
+                                <span className="tweet__username">SearchForATweet</span>
+                                <span className="tweet__handle">@SearchForATweet</span> &#8226;
+                                <span className="tweet__date">12 May 2019</span>
+                            </div>
+                            <div className="tweet_body_text">
+                                <p>Use the Search box below to search for a tweet from someone you follow! You can search by <em>@twitter_handle</em> or <em>tweet content</em>.</p>
+                                <input placeholder="@elonmusk or 'bananas" id="user-search"/>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="tweet">
+                        <div className="tweet_avatar">
+                            <img src="_assets/t-logo.png" alt=""/>
+                        </div>
+                        <div className="tweet__body">
+                            <div className="tweet_body_header">
+                                <span className="tweet__username">GrabATweet</span>
+                                <span className="tweet__handle">@grabATweet</span> &#8226;
+                                <span className="tweet__date">12 May 2019</span>
+                            </div>
+                            <div className="tweet_body_text">
+                                <p>Use the Grab-A-Tweet box below to search for a tweet from someone we follow! <em>#InspirationLaughsSelfDevelopment</em></p>
+                                <button type="button" id="random-search">Grab-A-Tweet</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </Fragment>
+        )
+    }
+}
+export default Home;
