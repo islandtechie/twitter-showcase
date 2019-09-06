2cdff451e03850f3370ccceac998b32d05e096e6
Berlin Smith <berlin.smithjr@gmail.com>
2019-09-06 18:09:38 -0400

    retriving access token from token broken

--- ApiService/AuthService.js
+++ ApiService/AuthService.js
@@ -1,25 +1,30 @@
 const axios = require('axios');
 require('dotenv').config();
+const grantType = "grant_type=client_credentials";
 
 let URL = "https://api.twitter.com/oauth2/token";
-let credentials = new Buffer(process.env.ACCESS_TOKEN + process.env.ACCESS_TOKEN_SECRET).toString('base64');
-let config = {
-    headers: {
-        "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
-        "Accept" : "application/json",
-        "Authorization": "Basic " + credentials,
-    },
-    body: "grant_type=client_credentials"
-};
 
 const getToken = async () => {
-    return await axios.post(URL, config)
-        .then(response => {
-            return response.data.access_token;
-        })
-        .catch(error => {
-            console.log(error.response.data);
-        });
-    };
+    console.log('token service called!');
+    return await axios.post(URL, grantType,
+    {
+      auth: {
+        username: process.env.TWITTER_API_KEY,
+        password: process.env.TWITTER_SECRET_KEY
+      },
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
+        "Accept": "application/json"
+      },
+      responseType: 'application/json',
+    })
+    .then(response => {
+      return response.data;
+      // console.log(response.data);
+    })
+    .catch(err => {
+      console.log(err.response.data);
+    });
+}
 
 module.exports = getToken;
